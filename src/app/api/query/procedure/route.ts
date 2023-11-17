f9fd507151e1d8ebb852763291f44b02376555ae
Raymond <six0860613@RaymonddeMacBook-Pro.local>
2023-11-17 14:55:39 +0800

    add routes: /api/procedure

---
+++ src/app/api/query/procedure/route.ts
@@ -0,0 +1,36 @@
+import sequelize from '@/lib/sequelize';
+import oracledb from 'oracledb';
+import { QueryTypes, DataTypes, Sequelize } from 'sequelize';
+import { NextResponse } from 'next/server';
+
+export const POST = async (req: Request) => {
+    const { input } = await req.json();
+    if (input === '') return NextResponse.json({ status: 400, result: 'Input Cannot Be Empty' });
+
+    try {
+        const queryStr = `
+        DECLARE
+            RX VARCHAR2(100);
+            o_order_code VARCHAR2(100);
+            o_msg VARCHAR2(20);
+        BEGIN
+          om001.ins_erp_order_header(
+            210,97,1,1,1,:RX,'','','','',1108,1,TO_DATE('20231108 14:37:00', 'yyyymmdd hh24:mi:ss'),
+            null, 'admin', :o_order_code, :o_msg);
+        END; `;
+
+        const res = await sequelize.query(queryStr, {
+            bind: {
+                RX: { type: oracledb.STRING, dir: oracledb.BIND_IN, val: input },
+                o_order_code: { type: oracledb.STRING, dir: oracledb.BIND_OUT },
+                o_msg: { type: oracledb.STRING, dir: oracledb.BIND_OUT },
+            },
+            type: QueryTypes.RAW,
+        });
+
+        return NextResponse.json({ status: 200, result: res });
+    } catch (error) {
+        console.log('error', error);
+        return NextResponse.json({ status: 404, result: 'Query is invalid' });
+    }
+};
--- src/app/query/page.tsx
+++ src/app/query/page.tsx
@@ -1,117 +1,144 @@
-"use client";
-import { Button } from "@/components/ui/button";
+'use client';
+import { Button } from '@/components/ui/button';
 import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "@/components/ui/table";
-import { Textarea } from "@/components/ui/textarea";
-import { useRef, useState } from "react";
+    Table,
+    TableBody,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+} from '@/components/ui/table';
+import { Textarea } from '@/components/ui/textarea';
+import { useRef, useState } from 'react';
 
 export default function Home() {
-  const [entries, setEntries] = useState([]);
-  const [headers, setHeaders] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [hasError, setHasError] = useState(null);
+    const [entries, setEntries] = useState([]);
+    const [headers, setHeaders] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+    const [hasError, setHasError] = useState(null);
 
-  const inputRef = useRef(null);
+    const inputRef = useRef<HTMLTextAreaElement>(null);
 
-  const submitHandler = async () => {
-    const input = inputRef.current!.value.trim();
-    if (!input) return;
-    setIsLoading(true);
-    const res = await fetch("/api/query/plaintext", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        input,
-      }),
-    });
-    const { status, result } = await res.json();
-    setIsLoading(false);
-    if (status === 200) {
-      setEntries(result[0]);
-      setHeaders(result[1]);
-      setHasError(null);
-    } else {
-      setHasError(result);
-    }
-  };
+    const submitTEST = async () => {
+        const input = inputRef.current!.value.trim();
+        if (!input) return;
+        setIsLoading(true);
+        const res = await fetch('/api/query/procedure', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                input,
+            }),
+        });
+        const { status, result } = await res.json();
+        setIsLoading(false);
+        if (status === 200) {
+            console.log('res', result);
+            setHasError(null);
+        } else {
+            setHasError(result);
+        }
+    };
 
-  const exportHandler = async () => {
-    if (entries.length === 0 || hasError) return;
+    const submitHandler = async () => {
+        const input = inputRef.current!.value.trim();
+        if (!input) return;
+        setIsLoading(true);
+        const res = await fetch('/api/query/plaintext', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                input,
+            }),
+        });
+        const { status, result } = await res.json();
+        setIsLoading(false);
+        if (status === 200) {
+            console.log('res', result);
+            setEntries(result[0]);
+            setHeaders(result[1]);
+            setHasError(null);
+        } else {
+            setHasError(result);
+        }
+    };
 
-    const res = await fetch("/api/report", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        headers,
-        entries,
-      }),
-    });
+    const exportHandler = async () => {
+        if (entries.length === 0 || hasError) return;
 
-    const blob = await res.blob();
+        const res = await fetch('/api/report', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                headers,
+                entries,
+            }),
+        });
 
-    const url = window.URL.createObjectURL(blob);
-    const a = document.createElement("a");
-    a.href = url;
-    a.download = "report.xlsx";
-    document.body.appendChild(a);
-    a.click();
-    document.body.removeChild(a);
-  };
+        const blob = await res.blob();
 
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="flex flex-row items-center w-full gap-6">
-        <Textarea disabled={isLoading} ref={inputRef} />
-        <Button disabled={isLoading} onClick={submitHandler}>
-          Submit
-        </Button>
-        <Button
-          disabled={isLoading || entries.length === 0 || !!hasError}
-          onClick={exportHandler}
-        >
-          Export
-        </Button>
-      </div>
-      {isLoading ? (
-        <span className="text-2xl">Loading ...</span>
-      ) : hasError ? (
-        <span className="text-red-500 text-2xl">{hasError}</span>
-      ) : (
-        entries.length !== 0 && (
-          <Table>
-            <TableHeader>
-              <TableRow>
-                {headers.map((header) => (
-                  <TableHead key={header.name} className="">
-                    {header.name}
-                  </TableHead>
-                ))}
-              </TableRow>
-            </TableHeader>
-            <TableBody>
-              {entries.map((entry, rowIndex) => (
-                <TableRow key={rowIndex}>
-                  {headers.map((header) => (
-                    <TableCell key={header.name} className="">
-                      {entry[header.name]}
-                    </TableCell>
-                  ))}
-                </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-        )
-      )}
-    </main>
-  );
+        const url = window.URL.createObjectURL(blob);
+        const a = document.createElement('a');
+        a.href = url;
+        a.download = 'report.xlsx';
+        document.body.appendChild(a);
+        a.click();
+        document.body.removeChild(a);
+    };
+
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            <div className="flex flex-row items-center w-full gap-6">
+                <Textarea disabled={isLoading} ref={inputRef} />
+                <Button disabled={isLoading} onClick={submitHandler}>
+                    Submit
+                </Button>
+                <Button
+                    disabled={isLoading || entries.length === 0 || !!hasError}
+                    onClick={exportHandler}
+                >
+                    Export
+                </Button>
+                <Button disabled={isLoading} onClick={submitTEST}>
+                    TEST
+                </Button>
+            </div>
+            {isLoading ? (
+                <span className="text-2xl">Loading ...</span>
+            ) : hasError ? (
+                <span className="text-red-500 text-2xl">{hasError}</span>
+            ) : (
+                entries.length !== 0 && (
+                    <Table>
+                        <TableHeader>
+                            <TableRow>
+                                {headers.map((header) => (
+                                    <TableHead key={header.name} className="">
+                                        {header.name}
+                                    </TableHead>
+                                ))}
+                            </TableRow>
+                        </TableHeader>
+                        <TableBody>
+                            {entries.map((entry, rowIndex) => (
+                                <TableRow key={rowIndex}>
+                                    {headers.map((header) => (
+                                        <TableCell key={header.name} className="">
+                                            {entry[header.name]}
+                                        </TableCell>
+                                    ))}
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                    </Table>
+                )
+            )}
+        </main>
+    );
 }
